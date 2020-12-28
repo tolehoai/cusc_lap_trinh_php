20e61ce461892bb52b92180f08c8cf50213a21aa
tolehoai <tolehoai@gmail.com>
2020-12-28 19:23:27 +0700

    add alert

--- backend/functions/loaisanpham/create.php
+++ backend/functions/loaisanpham/create.php
@@ -176,6 +176,25 @@
   <!-- <script src="..."></script> -->
   <script>
     $(document).ready(function() {
+      
+      Swal.fire({
+  title: '<strong>HTML <u>example</u></strong>',
+  icon: 'info',
+  html:
+    'You can use <b>bold text</b>, ' +
+    '<a href="//sweetalert2.github.io">links</a> ' +
+    'and other HTML tags',
+  showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText:
+    '<a href="html://google.com">Link</a>',
+  confirmButtonAriaLabel: 'Thumbs up, great!',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i>',
+  cancelButtonAriaLabel: 'Thumbs down'
+})
+
       $("#frmLoaiSanPham").validate({
         rules: {
           lsp_ten: {
--- backend/layouts/scripts.php
+++ backend/layouts/scripts.php
@@ -2,3 +2,4 @@
 <script src="/cusc_lap_trinh_php/backend/assets/vendor/jquery/jquery.min.js"></script>
 <script src="/cusc_lap_trinh_php/backend/assets/vendor/boostrap/js/bootstrap.min.js"></script>
 <script src="/cusc_lap_trinh_php/backend/assets/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
