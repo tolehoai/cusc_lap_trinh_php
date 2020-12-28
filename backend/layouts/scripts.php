e4ccddb2a14c1ef3bf76685ab443480163647aa6
tolehoai <tolehoai@gmail.com>
2020-12-28 18:16:56 +0700

    init

---
+++ backend/layouts/scripts.php
@@ -0,0 +1,4 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/tolehoai/backend/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/tolehoai/backend/assets/vendor/boostrap/js/bootstrap.min.js"></script>
+<script src="/tolehoai/backend/assets/vendor/jquery-validation/jquery.validate.min.js"></script>
---
+++ backend/layouts/styles.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="/tolehoai/backend/assets/vendor/boostrap/css/bootstrap.min.css" type="text/css" />
+<!-- Font awesome -->
+<!-- <link rel="stylesheet" href="/tolehoai/backend/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" /> -->
+
+<!-- Custom css - Các file css do chúng ta tự viết -->
+<!-- <link rel="stylesheet" href="http://localhost:1000/tolehoai/backend/assets/backend/css/style.css" type="text/css" /> -->
